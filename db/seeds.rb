a075c7a1d45eb144ea0802a52481acf207fc3f1a
Pavel Perkin <p1.8t@yandex.ru>
2017-03-03 03:29:31 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-Dialect.create( [ { name: 'Yoda', icon_name: 'yoda' },
-                  { name: 'Valley Girl', icon_name: 'valley' },
-                  { name: 'Binary Code', icon_name: 'binary' } ] )
-user1 = User.create( name: Faker::Friends.character )
+dialects = Dialect.create( [ { name: 'Yoda', icon_name: 'yoda' },
+                             { name: 'Valley Girl', icon_name: 'valley' },
+                             { name: 'Binary Code', icon_name: 'binary' } ] )
+user1 = User.create( name: Faker::Friends.character, dialect: dialects.sample )
 user1.messages.create( [ { body: Faker::Friends.quote },
                          { body: Faker::Friends.quote },
                          { body: Faker::Friends.quote },
@@ -9,7 +9,7 @@ user1.messages.create( [ { body: Faker::Friends.quote },
                          { body: Faker::Friends.quote },
                          { body: Faker::Friends.quote } ] )
 
-user2 = User.create( name: Faker::Friends.character )
+user2 = User.create( name: Faker::Friends.character, dialect: dialects.sample )
 user2.messages.create( [ { body: Faker::Friends.quote },
                          { body: Faker::Friends.quote },
                          { body: Faker::Friends.quote },
